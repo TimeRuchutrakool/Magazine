682e4122bfe29630a80ee982ec4a95dd8806f33f
tharmJaa <timeruchu0808@gmail.com>
2023-03-27 13:50:18 +0700

    Initial Commit

---
+++ Magazine/MagazineApp.swift
@@ -0,0 +1,17 @@
+//
+//  MagazineApp.swift
+//  Magazine
+//
+//  Created by Time Ruchutrakool on 3/27/23.
+//
+
+import SwiftUI
+
+@main
+struct MagazineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
